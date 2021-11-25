3d383955e64592632b5632b96ae3d6b393f0ae8a
bisaek <59936920+bisaek@users.noreply.github.com>
2021-11-25 12:03:02 +0100

    console log

--- firefox/index.js
+++ firefox/index.js
@@ -7,7 +7,7 @@ fetch('https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=AIzaS
     .then(res => {
         document.getElementsByClassName('top-level-buttons')[1].childNodes[1].firstChild.childNodes[1].innerHTML = 
             res.items[0].statistics.dislikeCount
-        
+        console.log(res.items[0].statistics.dislikeCount)
     }).catch(function(error) {
         console.log(error);
     });
