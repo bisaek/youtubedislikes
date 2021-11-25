2bbdbedc33fcad620a7463011b6078811b3c474b
bisaek <59936920+bisaek@users.noreply.github.com>
2021-11-25 11:35:18 +0100

    change disklike to the real disklike

--- firefox/index.js
+++ firefox/index.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName('top-level-buttons')[1].childNodes[1].firstChild.childNodes[1].innerHTML = "hej"
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString);
+videoId = urlParams.get('watch')
+
+fetch('https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=AIzaSyArZbYjKtxq-IGULCp3V9ol-Rnu_KgW2hE&part=statistics')
+    .then(res => res.json())
+    .then(res => {
+        document.getElementsByClassName('top-level-buttons')[1].childNodes[1].firstChild.childNodes[1].innerHTML = 
+            res.items[0].statistics.dislikeCount
+    })
